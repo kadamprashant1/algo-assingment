dc2dda030b14ce6c280cce8e3196b28c93261ec4
Prashant Kadam <kadamprashant9822@gmail.com>
2026-09-01 16:45:44 +0530

    qn 10 added

---
+++ question10.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isBalanced(const string& s) {
+    stack<char> st;
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);                      
+        }
+        else if (c == ')' || c == '}' || c == ']') {
+            if (st.empty()) return false;        
+
+            char top = st.top();
+            st.pop();
+
+            if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                return false;            
+            }
+        }
+    }
+    return st.empty();                
+
+int main() {
+    string s;
+    cout << "Enter string: ";
+    getline(cin, s);
+    if (isBalanced(s))
+        cout << "Balanced\n";
+    else
+        cout << "Not Balanced\n";
+    return 0;
+}
